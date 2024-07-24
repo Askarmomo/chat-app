d57b8562e4a3037b9f44b178015a680cf08856af
Askarmomo <Askarmomo70@gmail.com>
2024-07-24 17:06:24 +0530

    uploading

--- server/server.js
+++ server/server.js
@@ -1,4 +1,4 @@
-
+import path from 'path'
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
@@ -15,12 +15,20 @@ dotenv.config();
 // PORT should be assigned after calling dotenv.config() because we need to access the env variables. Didn't realize while recording the video. Sorry for the confusion.
 const PORT = process.env.PORT || 4000;
 
+const __dirname = path.resolve()
+
 app.use(express.json()); // to parse the incoming requests with JSON payloads (from req.body)
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
+
+app.use(express.static(path.join(__dirname,'/client/dist')))
+
+app.get('*',(req,res)=>{
+	res.sendFile(path.join(__dirname,'client','dist','index.html'))
+})
 
 server.listen(PORT, () => {
 	connectToMongoDB();
