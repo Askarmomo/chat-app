6ac612909462ca8186a0c7ad0d0e165450a14b5e
Mohammed Askar <askarmomo111@gmail.com>
2025-01-30 09:54:45 +0530

    some deployement error fixed

--- server/server.js
+++ server/server.js
@@ -12,8 +12,8 @@ dotenv.config()
 
 app.use(express.json())
 app.use(cors({
-    origin: "http://localhost:5173",
-    credentials: true
+    origin: "https://chat-r2apes06f-askars-projects-111a8034.vercel.app",
+    credentials: true,
 }))
 app.use(cookieParser())
 
