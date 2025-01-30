9d98f2e079a0b43da89f752245b02ae4a493b171
Mohammed Askar <155381552+Askarmomo@users.noreply.github.com>
2025-01-30 09:24:49 +0530

    Update Axios.js

--- client/src/middlewares/Axios.js
+++ client/src/middlewares/Axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:3000',
+    baseURL: 'https://chat-app-36uv.onrender.com',
     withCredentials: true,
 });
 
